ad1852d7a7fe0c3bf5a63e4ae06a42863efd3e3b
JSWJJ <gmldi10009@gmail.com>
2023-11-10 17:25:04 +0900

    예약 결제 페이지 제각(css제외)

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import { Route, Routes } from 'react-router-dom';
 import Main from './components/mainPage/main';
+import Yea from './components/Yea/Yea';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Main></Main>}></Route>
+        <Route path="/Yea" element={<Yea></Yea>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
