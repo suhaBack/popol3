c09c267c0d56012e8bce4528883c3e2d166af4dc
JUNHO <junho2088@gmail.com>
2023-11-10 15:44:16 +0900

    mypage 기본틀제작

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import { Route, Routes } from 'react-router-dom';
 import Main from './components/mainPage/main';
+import Mypage from './components/mypage/mypage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Main></Main>}></Route>
+        <Route path="/mypage" element={<Mypage></Mypage>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
