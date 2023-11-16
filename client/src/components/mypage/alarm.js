c2163471f0491190748bf111bca63d8d0b0453f7
JSWJJ <gmldi10009@gmail.com>
2023-11-16 17:47:19 +0900

    마이페이지 HTML수정중

--- client/src/components/mypage/alarm.js
+++ client/src/components/mypage/alarm.js
@@ -1,13 +1,40 @@
 function Alarm(){
   return (
-    <div>
+   <div>
+     <div>
       <div>
-
+        <span>알림 설정</span>
+        <p>특가, 쿠폰 등 이벤트 정보를 빠르게 알려드릴게요.</p>
       </div>
       <div>
-        
+        <span>앱 잠금, 최근 본 상품 저장, 앱 푸시, 접근 권한 설정은 여기어때 앱에서 가능해요.</span>
+      </div>
+      <div>
+        <div>
+          <form method='get'>
+           <label><input type="checkbox" name="color" value="blue"/> 마케팅 알림 수신 동의(선택)</label>
+          </form>
+        </div>
+      </div>
+      <div>
+        <div>
+          <form method='get'>
+           <label><input type="checkbox" name="color" value="blue"/> 문자</label>
+          </form>
+        </div>
+      </div>
+      <div>
+        <div>
+          <form method='get'>
+            <label><input type="checkbox" name="color" value="blue"/> 이메일</label>
+          </form>
+        </div>
       </div>
     </div>
+    <div>
+      <button>저장</button>
+    </div>
+   </div>
   )
 }
 
--- client/src/components/mypage/reservation.js
+++ client/src/components/mypage/reservation.js
@@ -1,17 +1,23 @@
-import img1 from './image/1.jpg'
+import img1 from './image/잠실체리.jpg'
+import img2 from './image/잠실쿠키.jpg'
+import img3 from './image/신림 홀리데이.jpg'
+import img4 from './image/상봉 호텔버스.jpg'
+import img5 from './image/용전 더휴식 노크인호텔.jpg'
+import './reservation.css'
+// import img6 from './image/수원역 썸.jpg'
 
 
 function Reservation(){
   return (
-    <div>
+    <div id='jsw_maincontainer'>
       <div>
         <span>예약 내역</span>
       </div>
-      <div>
+      <div id='jsw_subcontainer'>
         <div>
           <span>이용 내역</span>
         </div>
-        <div>
+        <div class="jsw_01">
           <div>
             <img src={img1}></img>
           </div>
@@ -22,15 +28,48 @@ function Reservation(){
             <span>다시 예약</span>
           </div>
         </div>
+        <div class="jsw_02">
+          <div>
+            <img src={img3}></img>
+          </div>
+          <div class="jsw_text">
+            <span>이용완료</span>
+            <h4>신림 홀리데이</h4>
+            <p>06.05 월 - 06.06 화 • 1박</p>
+            <span>다시 예약</span>
+          </div>
+        </div>
+        <div class="jsw_01">
+          <div>
+            <img src={img4}></img>
+          </div>
+          <div>
+            <span>이용완료</span>
+            <h4>상봉 호텔버스</h4>
+            <p>02.18 토 - 02.19 일 • 1박</p>
+            <span>다시 예약</span>
+          </div>
+        </div>
+        <div class="jsw_02">
+          <div>
+            <img src={img5}></img>
+          </div>
+          <div>
+            <span>이용완료</span>
+            <h4>용전 더휴식 노크인호텔</h4>
+            <p>06.27 월 - 06.28 화 • 1박</p>
+            <span>다시 예약</span>
+          </div>
+        </div>
       </div>
 
-      <div>
+      <div class='jsw_03'>
         <div>
           <span>취소 내역</span>
         </div>
         <div>
           <div>
-            
+            <img src={img2}></img>
           </div>
           <div>
             <span>예약취소</span>
