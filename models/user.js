5066c2b39ef8a62746421843bb41a3765fe0b084
no2thingmore <nangmuu00@gmail.com>
2023-11-16 09:27:17 +0900

    서버 models 추가

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
+// 사용자
+
 const Sequelize = require("sequelize");
-//id | pwd | CKpwd | email | name | phone
+
+// user_id || email || password || name 
+// contact_number || address || profile_image || role
+
 class User extends Sequelize.Model {
   static initiate(sequelize) {
     User.init({
@@ -7,52 +12,85 @@ class User extends Sequelize.Model {
         type: Sequelize.INTEGER,
         primaryKey: true,
         allowNull: false,
-        comment: "식별자",
+        unique: true,
+        comment: "사용자(user) 식별자 ID (기본키)",
       },
       email: {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true,
+        comment: "이메일",
         validate: {
           isEmail: {
-            msg: "Must be a valid email address",
+            msg: "이메일 형식에 맞지 않습니다.",
           }
-        }
+        },
       },
       password: {
         type: Sequelize.STRING,
         allowNull: false,
+        validate: {
+          is: {
+            args: /^.{6,}$/,
+            msg: "비밀번호는 최소 6자 이상이어야 합니다."
+          }
+        },
         comment: "비밀번호",
       },
       name: {
         type: Sequelize.STRING(100),
         allowNull: false,
+        validate: {
+          len: {
+            args: [2, 100],
+            msg: "이름은 최소 2자 이상이어야 합니다."
+          }
+        },
         comment: "이름",
       },
       contact_number: {
         type: Sequelize.STRING(50),
         allowNull: false,
+        unique: true,
+        validate: {
+          is: {
+            args: /^(\+\d{1,3}[- ]?)?\d{10}$/,
+            msg: "유효한 연락처 형식이 아닙니다."
+          }
+        },
         comment: "연락처",
       },
       address: {
         type: Sequelize.STRING,
         allowNull: false,
+        validate: {
+          len: {
+            args: [5, 255],
+            msg: "주소는 5자 이상이어야 합니다."
+          }
+        },
         comment: "주소",
       },
       profile_image: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: true,
         comment: "프로필 이미지",
       },
       role: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        comment: "사용자 역할",
+        validate: {
+          isIn: {
+            args: [[0, 1, 2]],
+            msg: "유효한 역할이 아닙니다."
+          }
+        },
+        comment: "0(일반회원), 1(사업자회원), 2(관리자)", // 사용자 역할
       },
     }, {
       sequelize,
-      timestamps: false,
-      underscored: false,
+      timestamps: true,
+      underscored: true,
       modelName: 'User',
       tableName: 'users',
       paranoid: false,
@@ -60,11 +98,6 @@ class User extends Sequelize.Model {
       collate: 'utf8_general_ci',
     });
   }
-
-  // static associate(db) {
-  //   db.User.hasMany(db.Cart, { foreignKey: 'userId', sourceKey: 'id'});
-  //   db.User.hasMany(db.Order, { foreignKey: 'userId', sourceKey: 'id'});
-  // }
 };
 
 module.exports = User;
