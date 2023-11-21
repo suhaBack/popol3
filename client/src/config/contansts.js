9bc89390c14e2a196a72f3ac12bc305acbfa85d6
GyeongIk <gmldi10009@gmail.com>
2023-11-21 18:34:06 +0900

    product 페이지 컴포넌트 생성

--- client/src/config/contansts.js
+++ client/src/config/contansts.js
@@ -1,3 +1,3 @@
 //db랑 서버를 연결할 메인컴퓨터 주소 선택
-// export const API_URL = "http://168.126.242.77:8080";
-export const API_URL = "http://localhost:8080";
+export const API_URL = "http://168.126.242.77:8080";
+// export const API_URL = "http://localhost:8080";
