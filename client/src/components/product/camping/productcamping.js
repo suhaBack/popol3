0f4b8615c24e50137d52f4fbee78c6cd624397af
GyeongIk <gmldi10009@gmail.com>
2023-11-22 22:16:40 +0900

    카테고리 페이지 틀

--- client/src/components/product/camping/productcamping.js
+++ client/src/components/product/camping/productcamping.js
@@ -1,11 +1,34 @@
 import "./productcamping.css";
+import ProductListC from "./productlistC";
+import StickyMenuC from "./stickymenuC";
 
 function ProductCamping(){
   return (
-    <div>
-      캠핑
+    <div className="hotel-container">
+    <div className="productpageTitle">
+      <div className="container">
+        <div className="productTitle">캠핑</div>
+        <div>
+          <span class="dropdown">
+            <span className="dropbtn">
+              지역선택하기 <i class="fa-solid fa-caret-down"></i>
+            </span>
+            <div class="dropdown-content" style={{ margin: 0 }}>
+              <a href="#">- 서울</a>
+              <a href="#">- 경기/인천</a>
+              <a href="#">- 강원</a>
+              <a href="#">- 대전</a>
+              <a href="#">- 부산</a>
+            </div>
+          </span>
+        </div>
+      </div>
     </div>
-  )
+    <div className="productpageGridBox container">
+      <StickyMenuC></StickyMenuC>
+      <ProductListC></ProductListC>
+    </div>
+  </div>
+);
 }
-
 export default ProductCamping;
--- client/src/components/product/guesthouse/productguesthouse.js
+++ client/src/components/product/guesthouse/productguesthouse.js
@@ -1,11 +1,35 @@
 import "./productguesthouse.css";
+import ProductListG from "./productlistG";
+import StickyMenuG from "./stickymenuG";
 
-function ProductGuesthouse(){
+function productguesthouse() {
   return (
-    <div>
-      게스트 하우스
+    <div className="hotel-container">
+      <div className="productpageTitle">
+        <div className="container">
+          <div className="productTitle">게스트하우스</div>
+          <div>
+            <span class="dropdown">
+              <span className="dropbtn">
+                지역선택하기 <i class="fa-solid fa-caret-down"></i>
+              </span>
+              <div class="dropdown-content" style={{ margin: 0 }}>
+                <a href="#">- 서울</a>
+                <a href="#">- 경기/인천</a>
+                <a href="#">- 강원</a>
+                <a href="#">- 대전</a>
+                <a href="#">- 부산</a>
+              </div>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="productpageGridBox container">
+        <StickyMenuG></StickyMenuG>
+        <ProductListG></ProductListG>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProductGuesthouse;
+export default productguesthouse;
