21b7bd22803c7cfb507d1e0b34e2ee9f097a337c
IkGy <138839229+IkGy@users.noreply.github.com>
2024-01-08 15:47:15 +0900

    백

--- routes/Rooms.js
+++ routes/Rooms.js
@@ -29,29 +29,54 @@ router
         lodging_id:req.query.lodging_id,
       }
     })
-    console.log('data',Roomsdata);
+    // console.log('data',Roomsdata);
     res.status(201).send(Roomsdata);
   } catch (error) {
     console.error(error);
     res.status(501).send(error);
   }
 })
-  .post("/", async (req, res, next) => {
-    try {
-      const newRooms = Room.create({
-        room_id: req.body.room_id,
-        lodging_id: req.body.lodging_id,
-        type: req.body.type,
-        price: req.body.price,
-        capacity: req.body.capacity,
-        description: req.body.description,
-      });
-      console.log(newRooms);
-      res.status(201).end();
-    } catch (error) {
-      console.error(error);
-      res.status(500).end();
-    }
-  });
-
+.get('/all',(req,res,next)=>{
+  Room.findAll().then((response)=>{
+    res.status(201).send(response)
+  })
+})
+.post("/", async (req, res, next) => {
+  try {
+    const newRooms = Room.create({
+      room_id: req.body.room_id,
+      lodging_id: req.body.lodging_id,
+      type: req.body.type,
+      price: req.body.price,
+      capacity: req.body.capacity,
+      description: req.body.description,
+    });
+    console.log(newRooms);
+    res.status(201).end();
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
+})
+.patch('/detail',async(req,res,next)=>{
+  // res.status(201).end()
+  
+    console.log(req.body);
+    const ePrice = req.body.object
+    const roomID = req.body.room_id
+    console.log('-----------tests',ePrice);
+    console.log('--------------id', req.body.room_id);
+    await Room.update({
+      price: ePrice,
+      updated_at:new Date(),
+    },{
+      where:{room_id:roomID}
+    }).then(()=>{
+  res.status(201).end();
+    })
+     .catch ((error)=>{
+    console.log(error);
+    res.status(501).end();
+  })
+})
 module.exports = router;
